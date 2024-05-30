996383bd754dbbc9835ef56de0685f70433fa283
curimm <pediart55@naver.com>
2024-05-30 17:44:38 +0900

    GenreContainer, sidebar완료

--- src/component/api.js
+++ src/component/api.js
@@ -1,12 +1,26 @@
 "use strict";
-//임시API key
+// 임시 API key
 const api_key = "829a43a98259bc44cae297489c7e3bba";
 const imageBaseURL = "https://image.tmdb.org/t/p/";
 
-const fetchDataFromServer = function(url, callback, optionalParam)
-{
-   fetch(url)
-   .then((response) => response.json())
-   .then((data) => callback(data.optionalParam));
+const fetchDataFromServer = function(url, callback) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => callback(data))
+    .catch((error) => console.error('Error fetching data:', error));
 };
-export {imageBaseURL, api_key,fetchDataFromServer};
+
+const fetchGenres = (callback) => {
+  fetchDataFromServer(
+    `https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}`,
+    ({ genres }) => {
+      const genreMap = {};
+      genres.forEach(({ id, name }) => {
+        genreMap[id] = name;
+      });
+      callback(genreMap);
+    }
+  );
+};
+
+export { imageBaseURL, api_key, fetchDataFromServer, fetchGenres };
--- src/component/container.js
+++ src/component/container.js
@@ -3,26 +3,6 @@ import React from 'react';
 import axios from "axios";
 import Content from './Content';
 function Container_All() {
-   class container_all extends React.Component{
-      state={
-         isLoading:true,
-         contents:[]
-      };
-   }
-   getContents = async () => {
-      try{
-         const{
-            data:{
-               data:{movies:contents} //샘플API Response에서 Movies를 contents로 사용 
-            }
-         } = await axios.get(
-            "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
-         );
-         this.setState({contents, isLoading})
-      }
-   }
-
-
   return (
     <article className="container_all">
       {/* 중앙 컨텐츠 내용 */}
