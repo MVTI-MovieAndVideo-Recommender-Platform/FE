21153e94d1cc7ea362b79e064d3d27435c926ee1
curimm <pediart55@naver.com>
2024-06-05 23:19:21 +0900

    수정0603

--- src/component/Layout.js
+++ src/component/Layout.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import SideBar from "./SideBar";
 import "../App.css";
 
-function Layout({ children }) {
+const Layout = ({children}) {
   return (
     <div className="App">
       <Header />
       <div className="App-body">
-        <SideBar />
         <main className="App-content">
           {children}
         </main>
@@ -20,3 +18,20 @@ function Layout({ children }) {
 }
 
 export default Layout;
+
+
+//function Layout({ children }) {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <div className="App-body">
+//         <main className="App-content">
+//           {children}
+//         </main>
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// }
+// 
+// export default Layout;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -30,34 +30,98 @@
         }
     
        render() {
-          const {isLoading, contents} = this.state;
-          return(
-             <section className='container01'>
-                {isLoading ? (
-                   <div className='loader'>
-                   <span className='loader__text'>추천목록 로딩중..</span>
-                   </div>
-                ):(
-                   <div className="contents">
-                     {contents.length > 0 ? (
-                        contents.map(content => (
-                          <Content
-                            key={content.id}
-                            id={content.id}
-                            year={content.year}
-                            title={content.title}
-                            summary={content.summary}
-                            poster={content.medium_cover_image}
-                            genres={content.genres}
-                          />
-                        ))
-                     ) : (
-                        <div>No contents available</div>
-                     )}
-                   </div>
-                )}
-             </section>
-          );
+         const{isLoading,contents} = this.state;
+         return(console);
+
+         
+//          const {isLoading, contents} = this.state;
+//          return(
+//             <section className='container01'>
+//                {isLoading ? (
+//                   <div className='loader'>
+//                   <span className='loader__text'>추천목록 로딩중..</span
+//                   </div>
+//                ):(
+//                   <div className="contents">
+//                     {contents.length > 0 ? (
+//                        contents.map(content => (
+//                          <Content
+//                            key={content.id}
+//                            id={content.id}
+//                            year={content.year}
+//                            title={content.title}
+//                            summary={content.summary}
+//                            poster={content.medium_cover_image}
+//                            genres={content.genres}
+//                          />
+//                        ))
+//                     ) : (
+//                        <div>No contents available</div>
+//                     )}
+//                   </div>
+//                )}
+//
+//             </section>
+//          );
        }
     }
     export default HomePage;
+
+//    class HomePage extends React.Component{
+//      state = {
+//         isLoading : true,
+//         contents:[]
+//      };
+//   
+//      getContents = async () => {
+//         try {
+//           const {
+//             data: {
+//               data: { movies: contents } // 샘플 API 응답에서 movies를 contents로 사용
+//             }
+//           } = await axios.get(
+//             "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+//           );
+//           this.setState({ contents, isLoading: false });
+//         } catch (error) {
+//           console.error("Error fetching data: ", error);
+//           this.setState({ isLoading: false });
+//         }
+//       };
+//       componentDidMount() {
+//         this.getContents();
+//       }
+//   
+//      render() {
+//         const {isLoading, contents} = this.state;
+//         return(
+//            <section className='container01'>
+//               {isLoading ? (
+//                  <div className='loader'>
+//                  <span className='loader__text'>추천목록 로딩중..</span>
+//                  </div>
+//               ):(
+//                  <div className="contents">
+//                    {contents.length > 0 ? (
+//                       contents.map(content => (
+//                         <Content
+//                           key={content.id}
+//                           id={content.id}
+//                           year={content.year}
+//                           title={content.title}
+//                           summary={content.summary}
+//                           poster={content.medium_cover_image}
+//                           genres={content.genres}
+//                         />
+//                       ))
+//                    ) : (
+//                       <div>No contents available</div>
+//                    )}
+//                  </div>
+//               )}
+//
+//            </section>
+//         );
+//      }
+//   }
+//   export default HomePage;
